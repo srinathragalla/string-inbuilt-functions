09ae08d1220713cee0c9103aa18a86eb254abfe9
srinath ragalla <srinathragalla17@gmail.com>
2021-12-14 23:23:24 +0530

    inbuilt functions

---
+++ str inbuit fun.js
@@ -0,0 +1,51 @@
+var myStr="we are checking the length of the string";
+console.log(myStr.length);
+
+
+
+var myStr="checking the index of the string";
+console.log(myStr.indexOf('of'));
+
+
+var myStr="getting the last index of the string";
+console.log(myStr.lastIndexOf('of'));
+
+
+var str="slicing the string to our required length";
+console.log(str.slice(0,7));
+console.log(str.slice(-7));
+console.log(str.slice(11));
+
+
+var str="creating a substring";
+console.log(str.substring(11,14));
+
+
+var str="converting to upper case";
+console.log(str.toUpperCase(myStr));
+
+
+var str="CONVERTING TO LOWER CASE";
+console.log(str.toLowerCase(myStr));
+
+
+var firstStr="string";
+var secondStr="concatination";
+console.log(firstStr.concat(' ',secondStr));
+console.log(firstStr.concat(secondStr));
+console.log(firstStr.concat('@',secondStr));
+
+
+
+var myStr="     trimm    ";
+console.log(myStr.trim());
+
+
+var str="acquiring the character at required place";
+console.log(str.charAt(8));
+
+
+var str="splitting the sentence into our reqirement";
+console.log(str.split(''));
+console.log(str.split(" "));
+console.log(str.split(','));
